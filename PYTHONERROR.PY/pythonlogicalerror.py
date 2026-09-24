54321a67e1d31b8fd082de535d84523fa5f166ac
Ujjwal Kumar <ujjwalsharmasharma09@gmail.com>
2026-09-24 18:57:14 +0530

    ...

---
+++ PYTHONERROR.PY/pythonlogicalerror.py
@@ -0,0 +1,11 @@
+a=10
+b=20
+if a==b :# in this the : is missing the cretd a syntax error
+
+ print("yes")
+else :
+ print("no")
+ 
+ #print(a/0)# it is zero divsion error the it give error but we can handle this error but syntax error handle nahi ho sakta
+ l=[10,20,30]
+ print(1[4])# it also give logical error because it has only 3 index but it call 4 index
